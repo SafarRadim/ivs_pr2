d7d63abaaa38a3c5dc13a154dd5400a4a7f383c8
VitZelinka <xzelin26@stud.fit.vutbr.cz>
2022-04-09 19:36:50 +0200

    Math lib initial commit

---
+++ src/ivsProjekt2/MathLib/MathFuncs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MathLib
+{
+    public static class MathFuncs
+    {
+        public static double Addition(double baseNum, double num)
+        {
+            return baseNum + num;
+        }
+
+        public static double Subtraction()
+        {
+            double sum = 0;
+
+            return sum;
+        }
+
+        public static double Multiplication()
+        {
+            double sum = 0;
+
+            return sum;
+        }
+        public static double Division()
+        {
+            double sum = 0;
+
+            return sum;
+        }
+        public static double Factorial()
+        {
+            double sum = 0;
+
+            return sum;
+        }
+
+        public static double Exponent()
+        {
+            double sum = 0;
+
+            return sum;
+        }
+        public static double Root()
+        {
+            double sum = 0;
+
+            return sum;
+        }
+    }
+}
