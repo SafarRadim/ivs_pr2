56b41b83b2051f703594ce04227a1698ac162617
VitZelinka <xzelin26@stud.fit.vutbr.cz>
2022-04-09 19:50:52 +0200

    MathLib tests initial commit

---
+++ src/ivsProjekt2/MathLibTests/MathTest.cs
@@ -0,0 +1,54 @@
+using NUnit.Framework;
+
+namespace MathLibTests
+{
+    public class MathTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void AdditionTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void SubtractionTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void MultiplicationTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void DivisionTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void FactorialTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ExponentTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void RootTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
