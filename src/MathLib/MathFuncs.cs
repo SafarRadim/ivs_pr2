d6b085242e6c703d3c509abe8ad332a59c8c1740
VitZelinka <xzelin26@stud.fit.vutbr.cz>
2022-04-27 19:57:10 +0200

    Implemented Factorial function

--- src/MathLib/MathFuncs.cs
+++ src/MathLib/MathFuncs.cs
@@ -51,20 +51,34 @@ namespace MathLib
 
             return sum;
         }
-        public static double Factorial()
+        public static double Factorial(double num)
+        {
+            if (num == 0) { return 1; }
+            if (num < 0 || (num%1) > 0)
+            {
+                throw new Exception("Invalid factorial value");
+            }
+
+            double sum = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                sum = sum * i;
+                if (double.IsInfinity(sum))
+                {
+                    throw new Exception("Value is too high");
+                }
+            }
+
+            return sum;
+        }
+
+        public static double Exponent(double baseNum, double power)
         {
             double sum = 0;
 
             return sum;
         }
-
-        public static double Exponent()
-        {
-            double sum = 0;
-
-            return sum;
-        }
-        public static double Root()
+        public static double Root(double baseNum, double degree)
         {
             double sum = 0;
 
