92816f974fb1eae1221ca2a7e3530ea8525c28bb
safar.radim <xsafar27@stud.fit.vutbr.cz>
2022-04-28 00:44:01 +0200

    Keyboard control Some logic changes

--- src/ivsProjekt2/MainWindow.xaml.cs
+++ src/ivsProjekt2/MainWindow.xaml.cs
@@ -1,18 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MathLib;
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MathLib;
 
 namespace ivsProjekt2
 {
@@ -24,40 +13,48 @@ namespace ivsProjekt2
         double firstNum;
         double secondNum;
         double outNum;
-        
+
         bool computed;
         bool equated;
         Operations lastOp;
 
-        enum Operations{
+        enum Operations
+        {
             addition,
             subtractition,
             multiplication,
             division,
             factorial,
             exponent,
-            root
+            root,
+            equals
         }
 
         public MainWindow()
         {
             InitializeComponent();
+            this.KeyDown += new KeyEventHandler(MainWindow_KeyDown);
         }
 
-        /// =============
+        /// =========
         /// 
-        /// Function keys
+        /// IO and logic
         /// 
-        /// =============
+        /// =========
 
-        private void CE_button_Click(object sender, RoutedEventArgs e)
+
+        /// --------------
+        /// Function logic
+        /// --------------
+
+        private void CE_logic()
         {
             OutputCurrentNumber.Text = String.Empty;
             firstNum = 0;
             secondNum = 0;
         }
 
-        private void C_button_Click(object sender, RoutedEventArgs e)
+        private void C_logic()
         {
             OutputCurrentEquation.Text = String.Empty;
             OutputCurrentNumber.Text = String.Empty;
@@ -65,60 +62,61 @@ namespace ivsProjekt2
             secondNum = 0;
         }
 
-        private void back_button_Click(object sender, RoutedEventArgs e)
+        private void back_logic()
         {
             OutputCurrentNumber.Text = OutputCurrentNumber.Text.Remove(OutputCurrentNumber.Text.Length - 1);
         }
 
-        private void func_button_Click(object sender, RoutedEventArgs e)
+        private void func_logic()
         {
-            /// TODO: Add func_button_Click logic 
-        }
-        
-        private void root_button_Click(object sender, RoutedEventArgs e)
-        {
-            /// TODO: add root_button_Click logic
+            /// TODO: Add func_logic()
         }
 
-        private void pow_button_Click(object sender, RoutedEventArgs e)
+        private void root_logic()
         {
-            /// TODO: add pow_button_Click logic
+            /// TODO: add root_logic()
         }
 
-        private void factorial_button_Click(object sender, RoutedEventArgs e)
+        private void pow_logic()
         {
-            /// TODO: add factorial_button_Click logic
+            /// TODO: add pow_logic()
         }
 
-        private void plus_button_Click(object sender, RoutedEventArgs e)
+        private void factorial_logic()
         {
-            if (String.IsNullOrEmpty(OutputCurrentNumber.Text) 
-                || OutputCurrentNumber.Text == "inf" 
+            /// TODO: add factorial_logic()
+        }
+
+        private void plus_logic()
+        {
+            if (String.IsNullOrEmpty(OutputCurrentNumber.Text)
+                || OutputCurrentNumber.Text == "inf"
                 || computed)
             {
                 return;
             }
-         
-            secondNum = Double.Parse(OutputCurrentNumber.Text);            
+
+            secondNum = Double.Parse(OutputCurrentNumber.Text);
 
             if (firstNum == 0)
             {
                 firstNum = secondNum;
                 OutputCurrentEquation.Text = secondNum.ToString() + " +";
                 OutputCurrentNumber.Text = String.Empty;
-                lastOp = Operations.addition;
-            } 
+            }
             else
             {
-                outNum = MathFuncs.Addition(firstNum, secondNum);
+                compute_last();
                 OutputCurrentEquation.Text = outNum.ToString() + " +";
                 OutputCurrentNumber.Text = outNum.ToString();
                 firstNum = outNum;
                 computed = true;
             }
+
+            lastOp = Operations.addition;
         }
 
-        private void minus_button_Click(object sender, RoutedEventArgs e)
+        private void minus_logic()
         {
             if (String.IsNullOrEmpty(OutputCurrentNumber.Text)
                 || OutputCurrentNumber.Text == "inf"
@@ -134,19 +132,20 @@ namespace ivsProjekt2
                 firstNum = secondNum;
                 OutputCurrentEquation.Text = secondNum.ToString() + " -";
                 OutputCurrentNumber.Text = String.Empty;
-                lastOp = Operations.subtractition;
             }
             else
             {
-                outNum = MathFuncs.Subtraction(firstNum, secondNum);
+                compute_last();
                 OutputCurrentEquation.Text = outNum.ToString() + " -";
                 OutputCurrentNumber.Text = outNum.ToString();
                 firstNum = outNum;
                 computed = true;
             }
+
+            lastOp = Operations.subtractition;
         }
 
-        private void multiply_button_Click(object sender, RoutedEventArgs e)
+        private void multiply_logic()
         {
             if (String.IsNullOrEmpty(OutputCurrentNumber.Text)
                 || OutputCurrentNumber.Text == "inf"
@@ -162,19 +161,20 @@ namespace ivsProjekt2
                 firstNum = secondNum;
                 OutputCurrentEquation.Text = secondNum.ToString() + " *";
                 OutputCurrentNumber.Text = String.Empty;
-                lastOp = Operations.multiplication;
             }
             else
             {
-                outNum = MathFuncs.Multiplication(firstNum, secondNum);
+                compute_last();
                 OutputCurrentEquation.Text = outNum.ToString() + " *";
                 OutputCurrentNumber.Text = outNum.ToString();
                 firstNum = outNum;
                 computed = true;
             }
+
+            lastOp = Operations.multiplication;
         }
 
-        private void division_button_Click(object sender, RoutedEventArgs e)
+        private void division_logic()
         {
             if (String.IsNullOrEmpty(OutputCurrentNumber.Text)
                 || OutputCurrentNumber.Text == "inf"
@@ -197,31 +197,19 @@ namespace ivsProjekt2
                 firstNum = secondNum;
                 OutputCurrentEquation.Text = secondNum.ToString() + " /";
                 OutputCurrentNumber.Text = String.Empty;
-                lastOp = Operations.division;
             }
             else
             {
-                outNum = MathFuncs.Division(firstNum, secondNum);
+                compute_last();
                 OutputCurrentEquation.Text = outNum.ToString() + " /";
                 OutputCurrentNumber.Text = outNum.ToString();
                 firstNum = outNum;
                 computed = true;
             }
+            lastOp = Operations.division;
         }
 
-        private void plusminus_button_Click(object sender, RoutedEventArgs e)
-        {
-            if (String.IsNullOrEmpty(OutputCurrentNumber.Text)
-                 || OutputCurrentNumber.Text == "inf"
-                 || computed)
-            {
-                return;
-            }
-
-            OutputCurrentNumber.Text = (Double.Parse(OutputCurrentNumber.Text) * -1).ToString();
-        }
-
-        private void dot_button_Click(object sender, RoutedEventArgs e)
+        private void plusminus_logic()
         {
             if (String.IsNullOrEmpty(OutputCurrentNumber.Text)
                 || OutputCurrentNumber.Text == "inf"
@@ -230,12 +218,26 @@ namespace ivsProjekt2
                 return;
             }
 
-            OutputCurrentNumber.Text += ",";
+            OutputCurrentNumber.Text = (Double.Parse(OutputCurrentNumber.Text) * -1).ToString();
         }
 
-        private void equals_button_Click(object sender, RoutedEventArgs e)
+        private void dot_logic()
         {
-            if (String.IsNullOrEmpty(OutputCurrentNumber.Text) 
+            if (String.IsNullOrEmpty(OutputCurrentNumber.Text)
+                || OutputCurrentNumber.Text == "inf"
+                || computed)
+            {
+                return;
+            }
+            if (OutputCurrentNumber.Text[OutputCurrentNumber.Text.Length - 1] != ',')
+            {
+                OutputCurrentNumber.Text += ",";
+            }
+        }
+
+        private void equals_logic()
+        {
+            if (String.IsNullOrEmpty(OutputCurrentNumber.Text)
                 || OutputCurrentNumber.Text == "inf")
             {
                 return;
@@ -279,18 +281,16 @@ namespace ivsProjekt2
 
             firstNum = outNum;
             OutputCurrentNumber.Text = outNum.ToString();
+            lastOp = Operations.equals;
             computed = true;
             equated = true;
-
         }
 
-        /// ===========
-        /// 
-        /// Number keys
-        /// 
-        /// ===========
+        /// ------------
+        /// number logic
+        /// ------------
 
-        private void one_button_Click(object sender, RoutedEventArgs e)
+        private void one_logic()
         {
             check_number();
 
@@ -303,7 +303,7 @@ namespace ivsProjekt2
             OutputCurrentNumber.Text += "1";
         }
 
-        private void two_button_Click(object sender, RoutedEventArgs e)
+        private void two_logic()
         {
             check_number();
 
@@ -316,7 +316,7 @@ namespace ivsProjekt2
             OutputCurrentNumber.Text += "2";
         }
 
-        private void three_button_Click(object sender, RoutedEventArgs e)
+        private void three_logic()
         {
             check_number();
 
@@ -329,7 +329,7 @@ namespace ivsProjekt2
             OutputCurrentNumber.Text += "3";
         }
 
-        private void four_button_Click(object sender, RoutedEventArgs e)
+        private void four_logic()
         {
             check_number();
 
@@ -342,7 +342,7 @@ namespace ivsProjekt2
             OutputCurrentNumber.Text += "4";
         }
 
-        private void five_button_Click(object sender, RoutedEventArgs e)
+        private void five_logic()
         {
             check_number();
 
@@ -355,7 +355,7 @@ namespace ivsProjekt2
             OutputCurrentNumber.Text += "5";
         }
 
-        private void six_button_Click(object sender, RoutedEventArgs e)
+        private void six_logic()
         {
             check_number();
 
@@ -368,7 +368,7 @@ namespace ivsProjekt2
             OutputCurrentNumber.Text += "6";
         }
 
-        private void seven_button_Click(object sender, RoutedEventArgs e)
+        private void seven_logic()
         {
             check_number();
 
@@ -381,7 +381,7 @@ namespace ivsProjekt2
             OutputCurrentNumber.Text += "7";
         }
 
-        private void eight_button_Click(object sender, RoutedEventArgs e)
+        private void eight_logic()
         {
             check_number();
 
@@ -394,7 +394,7 @@ namespace ivsProjekt2
             OutputCurrentNumber.Text += "8";
         }
 
-        private void nine_button_Click(object sender, RoutedEventArgs e)
+        private void nine_logic()
         {
             check_number();
 
@@ -407,7 +407,7 @@ namespace ivsProjekt2
             OutputCurrentNumber.Text += "9";
         }
 
-        private void zero_button_Click(object sender, RoutedEventArgs e)
+        private void zero_logic()
         {
             check_number();
 
@@ -420,13 +420,242 @@ namespace ivsProjekt2
             OutputCurrentNumber.Text += "0";
         }
 
+        /// =============
+        /// 
+        /// Function keys handle
+        /// 
+        /// =============
 
+        private void CE_button_Click(object sender, RoutedEventArgs e)
+        {
+            CE_logic();
+        }
+
+        private void C_button_Click(object sender, RoutedEventArgs e)
+        {
+            C_logic();
+        }
+
+        private void back_button_Click(object sender, RoutedEventArgs e)
+        {
+            back_logic();
+        }
+
+        private void func_button_Click(object sender, RoutedEventArgs e)
+        {
+            func_logic();
+        }
+
+        private void root_button_Click(object sender, RoutedEventArgs e)
+        {
+            root_logic();
+        }
+
+        private void pow_button_Click(object sender, RoutedEventArgs e)
+        {
+            pow_logic();
+        }
+
+        private void factorial_button_Click(object sender, RoutedEventArgs e)
+        {
+            factorial_logic();
+        }
+
+        private void plus_button_Click(object sender, RoutedEventArgs e)
+        {
+            plus_logic();
+        }
+
+        private void minus_button_Click(object sender, RoutedEventArgs e)
+        {
+            minus_logic();
+        }
+
+        private void multiply_button_Click(object sender, RoutedEventArgs e)
+        {
+            multiply_logic();
+        }
+
+        private void division_button_Click(object sender, RoutedEventArgs e)
+        {
+            division_logic();
+        }
+
+        private void plusminus_button_Click(object sender, RoutedEventArgs e)
+        {
+            plusminus_logic();
+        }
+
+        private void dot_button_Click(object sender, RoutedEventArgs e)
+        {
+            dot_logic();
+        }
+
+        private void equals_button_Click(object sender, RoutedEventArgs e)
+        {
+            equals_logic();
+        }
+
+        /// ===========
+        /// 
+        /// Number keys handle
+        /// 
+        /// ===========
+
+        private void one_button_Click(object sender, RoutedEventArgs e)
+        {
+            one_logic();
+        }
+
+        private void two_button_Click(object sender, RoutedEventArgs e)
+        {
+            two_logic();
+        }
+
+        private void three_button_Click(object sender, RoutedEventArgs e)
+        {
+            three_logic();
+        }
+
+        private void four_button_Click(object sender, RoutedEventArgs e)
+        {
+            four_logic();
+        }
+
+        private void five_button_Click(object sender, RoutedEventArgs e)
+        {
+            five_logic();
+        }
+
+        private void six_button_Click(object sender, RoutedEventArgs e)
+        {
+            six_logic();
+        }
+
+        private void seven_button_Click(object sender, RoutedEventArgs e)
+        {
+            seven_logic();
+        }
+
+        private void eight_button_Click(object sender, RoutedEventArgs e)
+        {
+            eight_logic();
+        }
+
+        private void nine_button_Click(object sender, RoutedEventArgs e)
+        {
+            nine_logic();
+        }
+
+        private void zero_button_Click(object sender, RoutedEventArgs e)
+        {
+            zero_logic();
+        }
+
+        /// =========
+        /// 
+        /// Keyboard handle
+        /// 
+        /// =========
+
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.NumPad1 || e.Key == Key.D1)
+            {
+                one_logic();
+            }
+
+            if (e.Key == Key.NumPad2 || e.Key == Key.D2)
+            {
+                two_logic();
+            }
+
+            if (e.Key == Key.NumPad3 || e.Key == Key.D3)
+            {
+                three_logic();
+            }
+
+            if (e.Key == Key.NumPad4 || e.Key == Key.D4)
+            {
+                four_logic();
+            }
+
+            if (e.Key == Key.NumPad5 || e.Key == Key.D5)
+            {
+                five_logic();
+            }
+
+            if (e.Key == Key.NumPad6 || e.Key == Key.D6)
+            {
+                six_logic();
+            }
+
+            if (e.Key == Key.NumPad7 || e.Key == Key.D7)
+            {
+                seven_logic();
+            }
+
+            if (e.Key == Key.NumPad8 || e.Key == Key.D8)
+            {
+                eight_logic();
+            }
+
+            if (e.Key == Key.NumPad9 || e.Key == Key.D9)
+            {
+                nine_logic();
+            }
+
+            if (e.Key == Key.NumPad0 || e.Key == Key.NumPad0)
+            {
+                zero_logic();
+            }
+
+            if (e.Key == Key.Add)
+            {
+                plus_logic();
+            }
+
+            if (e.Key == Key.Subtract)
+            {
+                minus_logic();
+            }
+
+            if (e.Key == Key.Multiply)
+            {
+                multiply_logic();
+            }
+
+            if (e.Key == Key.Divide)
+            {
+                division_logic();
+            }
+
+            if (e.Key == Key.Enter)
+            {
+                equals_logic();
+            }
+
+            if (e.Key == Key.Back)
+            {
+                back_logic();
+            }
+
+            if (e.Key == Key.Delete)
+            {
+                C_logic();
+            }
+
+            if (e.Key == Key.Decimal)
+            {
+                dot_logic();
+            }
+        }
         /// ======
         /// 
         /// Helper
         /// 
         /// ======
-        
+
         private void check_number()
         {
             if (computed)
@@ -441,6 +670,32 @@ namespace ivsProjekt2
                 OutputCurrentEquation.Text = String.Empty;
                 firstNum = 0;
                 equated = false;
+            }
+        }
+
+        private void compute_last()
+        {
+            switch (lastOp)
+            {
+                case Operations.addition:
+                    outNum = MathFuncs.Addition(firstNum, secondNum);
+                    firstNum = outNum;
+                    break;
+
+                case Operations.subtractition:
+                    outNum = MathFuncs.Subtraction(firstNum, secondNum);
+                    firstNum = outNum;
+                    break;
+
+                case Operations.multiplication:
+                    outNum = MathFuncs.Multiplication(firstNum, secondNum);
+                    firstNum = outNum;
+                    break;
+
+                case Operations.division:
+                    outNum = MathFuncs.Division(firstNum, secondNum);
+                    firstNum = outNum;
+                    break;
             }
         }
     }
