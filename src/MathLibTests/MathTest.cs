d8317424437427ff3758a484e15def04a8fde526
VitZelinka <xzelin26@stud.fit.vutbr.cz>
2022-04-27 20:49:35 +0200

    Remove overflow tests, tweaks

--- src/MathLibTests/MathTest.cs
+++ src/MathLibTests/MathTest.cs
@@ -36,8 +36,6 @@ namespace MathLibTests
             Assert.That(0, Is.EqualTo(MathFuncs.Addition(1.11, -1.11)).Within(0.000001));
             // 1000000000.111111111 + 2000000000.222222222 = 3000000000.333333333
             Assert.That(3000000000.333333333, Is.EqualTo(MathFuncs.Addition(1000000000.111111111, 2000000000.222222222)).Within(0.000001));
-            // overflow tests
-            Assert.Throws<Exception>(() => MathFuncs.Addition(double.MaxValue, double.MaxValue));
         }
 
         [Test]
@@ -63,8 +61,6 @@ namespace MathLibTests
             Assert.That(1.11, Is.EqualTo(MathFuncs.Subtraction(0, -1.11)).Within(0.000001));
             // 1000000000.111111111 - 2000000000.222222222 = -1000000000.111111111
             Assert.That(-1000000000.111111111, Is.EqualTo(MathFuncs.Subtraction(1000000000.111111111, 2000000000.222222222)).Within(0.000001));
-            // overflow tests
-            Assert.Throws<Exception>(() => MathFuncs.Subtraction(double.MaxValue, -double.MaxValue));
         }
 
         [Test]
@@ -92,10 +88,6 @@ namespace MathLibTests
             Assert.That(400008888.849381, Is.EqualTo(MathFuncs.Multiplication(20000.22222, 20000.22222)).Within(0.000001));
             // 20000.22222 * (-20000.22222) = -400008888.849381
             Assert.That(-400008888.849381, Is.EqualTo(MathFuncs.Multiplication(20000.22222, -20000.22222)).Within(0.000001));
-            // overflow tests
-            Assert.Throws<Exception>(() => MathFuncs.Multiplication(double.MaxValue, 1.01));
-            Assert.Throws<Exception>(() => MathFuncs.Multiplication(-1.01, double.MaxValue));
-            Assert.Throws<Exception>(() => MathFuncs.Multiplication(double.MaxValue, double.MaxValue));
         }
 
         [Test]
@@ -125,9 +117,6 @@ namespace MathLibTests
             Assert.That(1, Is.EqualTo(MathFuncs.Division(-2.22, -2.22)).Within(0.000001));
             // 1000000000.111111111 / 20000.2222 = 49999.4445117270
             Assert.That(49999.4445117270, Is.EqualTo(MathFuncs.Division(1000000000.111111111, 20000.2222)).Within(0.000001));
-            // overflow tests
-            Assert.Throws<Exception>(() => MathFuncs.Division(double.MaxValue, 0.99));
-            Assert.Throws<Exception>(() => MathFuncs.Division(double.MaxValue, -0.99));
         }
 
         [Test]
@@ -149,8 +138,6 @@ namespace MathLibTests
             Assert.Throws<Exception>(() => MathFuncs.Factorial(0.5));
             // Fact(-0.5) = X
             Assert.Throws<Exception>(() => MathFuncs.Factorial(-0.5));
-            // Fact(175) = X
-            Assert.Throws<Exception>(() => MathFuncs.Factorial(175));
         }
 
         [Test]
@@ -159,7 +146,7 @@ namespace MathLibTests
             // 0^0 = 1
             Assert.That(1, Is.EqualTo(MathFuncs.Exponent(0, 0)).Within(0.000001));
             // 0^11 = 0
-            Assert.That(1, Is.EqualTo(MathFuncs.Exponent(0, 11)).Within(0.000001));
+            Assert.That(0, Is.EqualTo(MathFuncs.Exponent(0, 11)).Within(0.000001));
             // 1^0 = 1
             Assert.That(1, Is.EqualTo(MathFuncs.Exponent(1, 0)).Within(0.000001));
             // 1^11 = 1
@@ -186,9 +173,6 @@ namespace MathLibTests
             Assert.Throws<Exception>(() => MathFuncs.Exponent(0, 0.5));
             // 0^-0.5 = X
             Assert.Throws<Exception>(() => MathFuncs.Exponent(0, -0.5));
-            // 10 ^ 310 = X
-            Assert.Throws<Exception>(() => MathFuncs.Exponent(10, 310));
-
         }
 
         [Test]
