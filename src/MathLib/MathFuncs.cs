0a858df9a11d2b868ff32c88a9837874cf213e2e
VitZelinka <xzelin26@stud.fit.vutbr.cz>
2022-04-28 23:22:33 +0200

    Implemented root function

--- src/MathLib/MathFuncs.cs
+++ src/MathLib/MathFuncs.cs
@@ -77,9 +77,37 @@ namespace MathLib
         }
         public static double Root(double baseNum, double degree)
         {
-            double sum = 0;
+            if (degree%1 > 0 || degree < 1)
+            {
+                throw new Exception("Invalid root degree");
+            }
 
-            return sum;
+            if (degree%2 == 0 && baseNum < 0)
+            {
+                throw new Exception("Number cannot be negative");
+            }
+
+            double change = double.MaxValue;
+            double precision = 0.000001;
+
+            double x = (baseNum / degree) / degree;
+            double last_x = x;
+
+            int iter = 0;
+
+            while (change > precision && iter < 1000)
+            {
+                x = (((degree - 1) / degree) * last_x) + ((baseNum / degree) * (1 / (Math.Pow(last_x, degree - 1))));
+                change = x - last_x;
+                if (change < 0)
+                {
+                    change = -change;
+                }
+                last_x = x;
+                iter++;
+            }
+            if (double.IsNaN(x)) { x = 0; }
+            return x;
         }
     }
 }
